d2cd0cb93eb1a0329d6f3c4a25ac16a734daafe2
Samrajya Thapa <samrajyathapa@hst000191857um.wifig1.olemiss.edu>
2020-04-08 01:11:29 -0500

    db

--- weatherApp/__init__.py
+++ weatherApp/__init__.py
@@ -4,8 +4,10 @@ from .weatherApp import db
 def create_app(test_config=None):
     app = Flask(__name__)
     app.secret_key='asdjnasdnasndsdas'
-    app.config['DEBUG'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:gasbt435@localhost/weatherapp'
+
+
+    app.config['DEBUG'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://pccwvjvplvkbyc:cf11d9ede7ef9995c8d553a99a1cecfc4e83bf303ea6ddec4c8da7bdf3979cdc@ec2-52-200-119-0.compute-1.amazonaws.com:5432/dagl2v5ed546h8'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     with app.app_context():
